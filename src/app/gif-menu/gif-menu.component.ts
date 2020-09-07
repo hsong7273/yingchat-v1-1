3d4dcd82d9fdbe4cc535bf79a9fa8d5d048f4384
hsong7273 <song.william7273@gmail.com>
2020-09-06 22:49:17 -0400

    first gif sticker

--- src/app/gif-menu/gif-menu.component.ts
+++ src/app/gif-menu/gif-menu.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { Message } from '../chat/messages';
+
+import { auth } from  'firebase/app';
+import { User } from  'firebase';
+
+import * as firebase from 'firebase/app';
+import { AuthenticationService } from  '../authentication.service';
 
 @Component({
   selector: 'app-gif-menu',
   templateUrl: './gif-menu.component.html',
   styleUrls: ['./gif-menu.component.css']
 })
-export class GifMenuComponent implements OnInit {
 
-  constructor() { }
+export class GifMenuComponent implements OnInit {
+  newMessage: Message;
+  itemsRef: AngularFireList<any>;
+  messages: Observable<any[]>;
+
+  constructor( db: AngularFireDatabase,
+  public authService: AuthenticationService ) {
+    this.itemsRef = db.list('messages');
+    this.messages = db.list('messages').valueChanges();
+   }
 
   ngOnInit() {
+    this.newMessage = new Message();
+  }
+
+  sendSticker(stickerID: string) {
+    var user = auth().currentUser;
+    var unameRef = firebase.database().ref('users/'+user.uid);
+    var test = unameRef.on('value', (snapshot)=> {
+      var uname = snapshot.val().uname;
+      this.newMessage.author = uname;
+      this.newMessage.date = String(new Date());
+      this.newMessage.text = "@Sticker_"+stickerID;
+      this.itemsRef.push( this.newMessage );
+    })
   }
 
 }
