b9f7ad18d199459bf8913663f3b9b4434d11f8d8
hsong7273 <song.william7273@gmail.com>
2020-09-07 11:19:58 -0400

    username database

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { AngularFireDatabase, AngularFireList } from "@angular/fire/database";
 import { Observable } from "rxjs";
 import { Message, MessageType } from "./messages";
+import { AngularFireAuth } from  "@angular/fire/auth";
 
 import { auth } from "firebase/app";
 import { User } from "firebase";
@@ -14,6 +15,7 @@ import { AuthenticationService } from "../authentication.service";
   templateUrl: "./chat.component.html",
   styleUrls: ["./chat.component.css"]
 })
+
 export class ChatComponent implements OnInit {
   public MessageTypeEnum = MessageType;
   newMessage: Message;
@@ -24,8 +26,8 @@ export class ChatComponent implements OnInit {
 
   constructor(
     db: AngularFireDatabase,
-    public authService: AuthenticationService
-  ) {
+    public authService: AuthenticationService,
+    private afAuth: AngularFireAuth) {
     this.itemsRef = db.list("messages");
     this.messages = db.list("messages").valueChanges();
   }
@@ -39,21 +41,30 @@ export class ChatComponent implements OnInit {
   }
 
   addItem() {
-    // var unameRef = firebase.database().ref("users/" + user.uid);
-    // var test = unameRef.on("value", snapshot => {
-    //   var uname = snapshot.val().uname;
-    //   // console.log(uname)
-    this.newMessage.author = auth().currentUser.email;
-    this.newMessage.date = String(new Date());
+    // this.afAuth.authState.switchMap(auth => {
+    //   if (auth) {
+    //     this.currentUser = new User(auth)
+    //     return this.db.object(`/users/${auth.uid}`)
+    //   } else return [];
+    // }).subscribe(user => {
+    //   this.currentUser['username'] = user.username
+    // })
+    var user = auth().currentUser;
+    var unameRef = firebase.database().ref('users/'+user.uid);
+    var test = unameRef.on('value', (snapshot)=> {
+      var uname = snapshot.val().uname;
+      this.newMessage.author = uname;
+      this.newMessage.date = String(new Date());
 
-    if (this.newMessage.text.match(/^@Sticker.*$/)) {
-      this.newMessage.type = MessageType.Gif;
-    } else {
-      this.newMessage.type = MessageType.Text;
-    }
+      if (this.newMessage.text.match(/^@Sticker.*$/)) {
+        this.newMessage.type = MessageType.Gif;
+      } else {
+        this.newMessage.type = MessageType.Text;
+      }
 
-    this.itemsRef.push(this.newMessage);
-    this.newMessage.text = "";
+      this.itemsRef.push(this.newMessage);
+      this.newMessage.text = "";
+    })
   }
 
   getGifPath(sticker: string): string {
